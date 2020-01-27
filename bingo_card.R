9db208c13aef53cb7886d4abdc58d51b25236d22
Erik Bolch <ebolch@ucmerced.edu>
2020-01-27 11:28:07 -0800

    Update 21-01-2020

---
+++ bingo_card.R
@@ -0,0 +1,30 @@
+## Title : Grading assignments Bingo card
+## Author : Erik Bolch
+c(
+  "graded the best student first",
+  "no name on paper",
+  "handwriting is intentionally unreadable",
+  "assignment submitted incorrectly",
+  "apology for how bad an answer is",
+  "clearly has no idea what the question was asking",
+  "did not follow instructions",
+  "answered all questions correctly and concisely",
+  "asked for extension",
+  "Peony",
+  "Daisy",
+  "Alstromeria",
+  "Lavender",
+  "Hibiscus",
+  "Birds of Paradise",
+  "Moonflower",
+  "Pansy",
+  "Hyacinth",
+  "Petunia",
+  "Poppy",
+  "Snapdragon",
+  "Violet",
+  "Cosmos",
+  "Calendula",
+  "Azalea",
+  "Begonia"
+)
